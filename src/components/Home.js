97c4bacfe9b38379be63e919f37a8b10245ab7ae
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-04-01 00:54:00 -0400

    feat: text displays on top of image There is text on the lower part of the image with styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import BackgroundImage from "./BackgroundImage/BackgroundImage";
+import BackgroundText from "./BackgroundText/BackgroundText";
 const Home = () => {
   return (
     <Fragment>
       <div style={{}}>
+        <BackgroundText />
         <BackgroundImage />
       </div>
     </Fragment>
