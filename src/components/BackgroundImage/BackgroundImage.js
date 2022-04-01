8d929c518762b5cdc80e07f3f5558984cff96e26
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-03-31 23:57:01 -0400

    feat: add background image Add background image and page fades in when it loads

---
+++ src/components/BackgroundImage/BackgroundImage.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+
+const BackgroundImage = () => {
+  return (
+    <Fragment>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          width={1775}
+          height={2220}
+          alt={"background"}
+          style={{ maxWidth: "70%", height: "auto" }}
+          src={
+            "https://cdn.discordapp.com/attachments/958975018816131132/959290872514621450/vlad.webp"
+          }
+        ></img>
+      </div>
+    </Fragment>
+  );
+};
+export default BackgroundImage;
