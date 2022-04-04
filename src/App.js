4f50eb96fe98d8e1d46d401f97fc0bfd9a9b51cf
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-04-04 04:17:21 -0400

    feat: add twitter timeline, clean up code Add a twitter timeline for blue jays twitter account, clean up uneeded code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./components/Home";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./components/NotFound/NotFound";
-import PSticky from "./components/PSticky";
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +10,6 @@ function App() {
         <Routes>
           <Route path="*" element={<NotFound />} />
           <Route path="/promo/" element={<Home />} />
-          <Route path="/promo2/" element={<PSticky />} />
           {/* <Route path="/news/guide/:article" element={<ArticleGuidePage />} /> */}
         </Routes>
       </BrowserRouter>
