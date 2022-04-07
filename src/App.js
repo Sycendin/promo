ef76fefa454c1ea2565b6a4226aafcb79c13d64e
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-04-07 00:04:48 -0400

    feat: add intro page Add initial intro page that plays on first visit to site

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from "./components/Home";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./components/NotFound/NotFound";
-
+import Intro from "./components/Intro/Intro";
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="*" element={<NotFound />} />
           <Route path="/promo/" element={<Home />} />
+          <Route path="/promo2/" element={<Intro />} />
           {/* <Route path="/news/guide/:article" element={<ArticleGuidePage />} /> */}
         </Routes>
       </BrowserRouter>
