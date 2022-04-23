ba2842c79a5dc6e5a57ceffdfa8b386d3b10c7c6
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-04-23 01:01:04 -0400

    update: add all player types Add all of the roster with some styling

--- src/components/CardDisplay/CardList.js
+++ src/components/CardDisplay/CardList.js
@@ -1,14 +1,21 @@
 import React, { Fragment } from "react";
 import "./CardList.css";
-import rosterData from ".../src/rosterData";
+import { fielders, relievers, starters } from "../../rosterData";
+import Card from "./Card/Card";
 const CardList = () => {
+  const allData = [fielders, relievers, starters];
+
   return (
     <Fragment>
-      <div className="cardlist-div">
-        {rosterData.map((data, i) => {
-          return <Card key={i} card={rosterData[i]}></Card>;
-        })}
-      </div>
+      {allData.map((playerType, i) => {
+        return (
+          <div key={i} className="cardlist-div">
+            {playerType.map((data, i) => {
+              return <Card key={i} card={playerType[i]}></Card>;
+            })}
+          </div>
+        );
+      })}
     </Fragment>
   );
 };
