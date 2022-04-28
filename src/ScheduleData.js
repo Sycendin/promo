c292d7e2598eb1db77a8e6c32004cf947248c7f6
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-04-28 01:05:42 -0400

    feat: schedule page displays all schedule images

---
+++ src/ScheduleData.js
@@ -0,0 +1,26 @@
+export const scheduleData = [
+  {
+    image:
+      "https://cdn.discordapp.com/attachments/958975018816131132/969095383743619132/march.webp",
+  },
+  {
+    image:
+      "https://cdn.discordapp.com/attachments/958975018816131132/969095384100143144/may.webp",
+  },
+  {
+    image:
+      "https://cdn.discordapp.com/attachments/958975018816131132/969095383563259924/june.webp",
+  },
+  {
+    image:
+      "https://cdn.discordapp.com/attachments/958975018816131132/969095383345152091/july.webp",
+  },
+  {
+    image:
+      "https://cdn.discordapp.com/attachments/958975018816131132/969095383072538684/august.webp",
+  },
+  {
+    image:
+      "https://cdn.discordapp.com/attachments/958975018816131132/969095384276299836/sept_oct.webp",
+  },
+];
--- src/components/CardDisplay/CardList.js
+++ src/components/CardDisplay/CardList.js
@@ -1,38 +1,60 @@
 import React, { Fragment } from "react";
 import "./CardList.css";
 import { fielders, relievers, starters } from "../../rosterData";
+import { scheduleData } from "../../ScheduleData";
 import Card from "./Card/Card";
 const CardList = ({ type }) => {
-  const allData = [];
+  let allData = [];
   if (type === "roster") {
     allData = [fielders, relievers, starters];
   } else if (type === "schedule") {
-    allData = [];
+    allData = [scheduleData];
   }
   return (
     <Fragment>
       {/* Loop through the 3 data sets */}
       {allData.map((playerType, i) => {
         return (
-          <div className="type-div">
-            {/* Return a div that has the title of that data set */}
+          <div key={i} className="type-div">
+            {/* Return a div that has the title depending on type */}
             <div className="cardlist-title">
               {" "}
-              {i === 0 ? (
+              {(i === 0) & (type === "roster") ? (
                 <p>Fielders</p>
-              ) : i === 1 ? (
+              ) : (i === 1) & (type === "roster") ? (
                 <p>Relievers</p>
-              ) : (
+              ) : (i === 3) & (type === "roster") ? (
                 <p>Starters</p>
+              ) : (
+                <p>Schedule</p>
               )}
             </div>
+            {/* --- */}
             {/* Return a div that contains a Card component for each player in
-            the data set */}
-            <div key={i} className="cardlist-div">
-              {playerType.map((data, i) => {
-                return <Card key={i} card={playerType[i]}></Card>;
-              })}
-            </div>
+            the data set if type is roster */}
+            {type === "roster" ? (
+              <div key={i} className="cardlist-div">
+                {playerType.map((data, i) => {
+                  return <Card key={i} card={playerType[i]} />;
+                })}
+              </div>
+            ) : (
+              // Return img htmls instead if on schedule page
+              <div key={playerType[i].image} className="cardlist-div">
+                {playerType.map((data, i) => {
+                  return (
+                    <img
+                      className="schedule-img"
+                      key={playerType[i].image}
+                      src={playerType[i].image}
+                      width={600}
+                      height={745}
+                      alt="schedule"
+                    />
+                  );
+                })}
+              </div>
+            )}
           </div>
         );
       })}
